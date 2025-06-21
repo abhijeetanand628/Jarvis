dfcc8bc9f54806fa9e55927b23cb3041e39e9fdb
Abhijeet Anand <abhijeetanand628@gmail.com>
2025-06-21 20:59:03 +0530

    Started

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{flex: 1, marginTop: "50%", alignItems: 'center'}}>
+      <Text style={{fontSize: 30 }}>Jarvis</Text>
+    </View>
+  );
+}
